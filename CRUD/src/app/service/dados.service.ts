d60e19b6163db79831c2216c41335383de576fea
Renato-napoli <renato.napoli75@gmail.com>
2023-12-28 16:35:21 -0300

    Feature: Dados service

--- CRUD/src/app/service/dados.service.ts
+++ CRUD/src/app/service/dados.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DadosService {
+private dados = [
+  {id: 1, nome: 'João', dataNascimento: '1990-01-01', classificacao: 'A'},
+  {id: 1, nome: 'Maria', dataNascimento: '1985-05-15', classificacao: 'B'}
+]
+  obterDados(): Observable<any[]> {
+    return of(this.dados)
+  }
 
-  constructor() { }
+  obterDadoPorId(id: number): Observable<any> {
+    const dado = this.dados.find(d => d.id === id);
+    return of(dado)
+  }
+
+  salvarDado(dado: any): Observable<any> {
+    return of(dado)
+  }
+
+  excluirDado(id: number): Observable<any> {
+    return of(null)
+  }
+
 }
