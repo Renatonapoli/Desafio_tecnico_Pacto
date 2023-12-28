b97e4e5252f796abcdf1a0ca1cca750bbe18e77b
Renato-napoli <renato.napoli75@gmail.com>
2023-12-28 17:01:02 -0300

    Feature: Formulario componente ts

--- CRUD/src/app/components/formulario/formulario.component.ts
+++ CRUD/src/app/components/formulario/formulario.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'
+import { DadosService } from 'src/app/service/dados.service';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit {
+  id: number | undefined
+  nome: string = ''
+  dataNascimento: string = ''
+  classificacao: string = ''
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private dadosService: DadosService
+  ){}
+
+  ngOnInit(): void {
+      this.route.params.subscribe(params => {
+        this.id =+ params['id']
+        if(this.id) {
+          this.carregaDado()
+        }
+      })
+  }
+
+  carregaDado() {
+    this.dadosService.obterDadoPorId(this.id!).subscribe(dado => {
+      this.nome = dado.none;
+      this.dataNascimento = dado.dataNascimento
+      this.classificacao = dado.classificacao
+    })
+  }
+
+  salvar() {
+    const dado = {
+      id: this.id,
+      nome: this.nome,
+      dataNascimento: this.dataNascimento,
+      classificacao :this.classificacao
+    }
+
+    this.dadosService.salvarDado(dado).subscribe(() => {
+      console.log('Dado salvo com sucesso')
+      this.router.navigate(['/lista'])
+    })
+  }
+
+  excluir() {
+    this.dadosService.excluirDado(this.id!).subscribe(() => {
+      console.log('Dado excluído com sucesso!')
+      this.router.navigate(['./lista'])
+    })
+  }
 }
