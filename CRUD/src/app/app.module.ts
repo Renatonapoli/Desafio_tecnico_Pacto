36c7929da729778578b727323eedb26434972ef4
Renato-napoli <renato.napoli75@gmail.com>
2023-12-28 16:15:37 -0300

    Feature: Componente lista

--- CRUD/src/app/app.module.ts
+++ CRUD/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { ListaComponent } from './components/lista/lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
