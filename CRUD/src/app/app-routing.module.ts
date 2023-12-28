4e86ee93e9942e706420a45c48ff274113c9664e
Renato-napoli <renato.napoli75@gmail.com>
2023-12-28 17:12:01 -0300

    Feature: Configuracao das rotas

--- CRUD/src/app/app-routing.module.ts
+++ CRUD/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaComponent } from './components/lista/lista.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'lista', component: ListaComponent},
+  {path: 'formulario/:id', component: FormularioComponent},
+  {path: '', redirectTo: '/lista', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
