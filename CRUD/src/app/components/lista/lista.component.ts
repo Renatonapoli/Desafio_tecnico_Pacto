17b40ef4eb5832b8cf6288aa711e161c37d17753
Renato-napoli <renato.napoli75@gmail.com>
2023-12-28 16:38:39 -0300

    Feature: Lista componente ts

--- CRUD/src/app/components/lista/lista.component.ts
+++ CRUD/src/app/components/lista/lista.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DadosService } from 'src/app/service/dados.service';
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
   styleUrls: ['./lista.component.css']
 })
 export class ListaComponent {
+  dados: any[] = [];
 
+  constructor(private dadosService: DadosService) {}
+
+  ngOnInit(): void {
+    this.obterDados()
+  }
+
+  obterDados() {
+    this.dadosService.obterDados().subscribe(dados => {
+      this.dados = dados
+    })
+  }
 }
