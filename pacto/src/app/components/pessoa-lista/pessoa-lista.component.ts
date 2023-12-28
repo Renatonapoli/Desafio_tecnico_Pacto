88f0983dc7e7e287bafcc4fac777a41bcf03368a
Renato-napoli <renato.napoli75@gmail.com>
2023-12-28 10:57:33 -0300

    Feature: Pessoa lista component

--- pacto/src/app/components/pessoa-lista/pessoa-lista.component.ts
+++ pacto/src/app/components/pessoa-lista/pessoa-lista.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pessoa } from '../../model/peessoa.model';
+import { PessoaService } from '../../service/pessoa.service';
 
 @Component({
   selector: 'app-pessoa-lista',
   templateUrl: './pessoa-lista.component.html',
-  styleUrls: ['./pessoa-lista.component.css']
+  styleUrls: ['./pessoa-lista.component.css'],
 })
-export class PessoaListaComponent {
+export class PessoaListaComponent implements OnInit {
+  pessoas: Pessoa[] = [];
 
+  constructor(private pessoaService: PessoaService) {}
+
+  ngOnInit(): void {
+    this.carregarPessoas();
+  }
+
+  carregarPessoas(): void {
+    this.pessoaService.listarPessoas().subscribe((pessoas) => {
+      this.pessoas = pessoas;
+    });
+  }
 }
